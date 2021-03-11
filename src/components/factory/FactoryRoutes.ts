5f83bee8327b16bb34fdbcfe1ce363c6113c1b3d
Manoj More <manoj.more@neosofttech.com>
2021-03-11 15:41:05 +0530

    refactor code

--- src/components/factory/FactoryRoutes.ts
+++ src/components/factory/FactoryRoutes.ts
@@ -9,6 +9,20 @@ export function factoryRoutes(app: express.Express, router: any) : any {
 
 
 
+    const GetCustomerByExtId = async (request: express.Request, response: express.Response, next: any) => {
+        try {
+            const controller: FactoryController = new FactoryController();
+            const result: any = await controller.GetCustomerByExtId(request.body);
+            response.locals.data = result.data;
+            ResponseHandler.JSONSUCCESS(request, response);
+
+        } catch (err) {
+            response.locals.errorCode = StatusCodes.UNAUTHORIZED;
+            response.locals.errors = err.message;
+            response.locals.error = err;
+            ResponseHandler.JSONERROR(request, response);
+        }
+    }
     const getFactory = async (request: express.Request, response: express.Response, next: any) => {
         try {
             const controller: FactoryController = new FactoryController();
@@ -40,6 +54,7 @@ export function factoryRoutes(app: express.Express, router: any) : any {
     }
 
 
+    router.get('/factory/GetCustomerByExtId', GetCustomerByExtId);
     router.get('/factory/:id', getFactory);
     router.post('/factory', saveFactory);
 }
